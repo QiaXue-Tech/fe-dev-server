c9b3a79f9e0bc658776dc635d9ab95e26ee7be6a
lowequincy <luowei05010@gmail.com>
2018-05-13 23:55:17 +0800

    支持npm安装

--- index.js
+++ index.js
@@ -1,37 +1,116 @@
 'use strict';
-const FeDevServer = require('./server');
-const app = new FeDevServer();
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const router = require('../routes');
+const fileType = require('../config/filetype');
+class FeDevServer {
+    constructor() {
+        this.handle = {};
+        this.router = router;
+    }
 
-app.get('/', (req, res) => {
-    res.writeHead(200, {
-        'Content-Type': 'text/html'
-    });
-    res.write('<head><meta charset="utf-8"/></head>');
-    res.end('访问了 /');
-});
-app.get('/start', (req, res) => {
-    res.writeHead(200, {
-        'Content-Type': 'text/html'
-    });
-    res.write('<head><meta charset="utf-8"/></head>');
-    res.end('访问了 /start');
-});
+    /**
+     * 启动服务器
+     * @param {Number} port 端口号默认8080
+     * @param {Function} callback 回调函数
+     */
+    runServer(port, callback, handle) {
+        const self = this;
 
-app.post('/api/banner',(req, res, data, err) => {
-   if(err) {
-       console.error(err);
-   } 
-   res.end(data);
-});
+        if (isNaN(Number(port))) {
+            callback = port;
+        }
 
-app.post('/user', (req, res)=>{
-    res.writeHead(200, {
-        'Content-Type': 'text/html'
-    });
-    res.write('<head><meta charset="utf-8"/></head>');
-    res.end('访问了 /user');
-})
+        if (typeof callback !== 'function') {
+            callback = null;
+        }
 
-app.renderView('/index', '/views/index.html');
+        const server = http.createServer((req, res) => {
+            const pathName = url.parse(req.url).pathname;
+            if (pathName === '/favicon.ico') {
+                res.writeHead(200);
+                res.end();
+                return;
+            }
 
-app.runServer();
+            if(self.getStaticResource(pathName, res)) {
+                return;
+            }
+
+            self.router.route(self.handle, pathName, res, req);
+        });
+
+        server.listen(Number(port) || 8080, callback);
+    }
+    
+    /**
+     * 
+     * @param {String} pathName 
+     * @param {ServerResponse} response 
+     */
+    getStaticResource(pathName, response) {
+        const self = this;
+        const suffixIndex = pathName.lastIndexOf('.');
+        if(suffixIndex > 0) {
+            const suffix = pathName.substring(suffixIndex);
+            const contentType = fileType[suffix];
+            const buffer = fs.readFileSync(`.${pathName}`);
+            response.writeHead(200, { 'Content-Type': contentType });
+            response.end(buffer);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 注册 Post 请求
+     * @param {String} pathName 请求地址
+     * @param {Function} callback 回调函数
+     */
+    post(pathName, callback) {
+        const self = this;
+        self.handle[pathName] = {
+            method: 'post',
+            callback: callback
+        };
+    }
+
+    /**
+     * 注册 Get 请求
+     * @param {String} pathName 请求地址
+     * @param {Function} callback 回调函数
+     */
+    get(pathName, callback) {
+        const self = this;
+        self.handle[pathName] = {
+            method: 'get',
+            callback: callback
+        };
+    }
+
+    /**
+     * 加载页面
+     * @param {String} pathName 请求地址
+     * @param {String} viewPath 视图路径
+     */
+    renderView(pathName, viewPath) {
+        const self = this;
+        self.handle[pathName] = {
+            method: 'get',
+            callback: (req, res) => {
+                res.setHeader('Content-Type','text/html;charset=utf-8');
+                fs.readFile(`.${viewPath}`, (err, data) => {
+                    if(err) {
+                        res.writeHead(404);
+                        res.end();
+                    }
+
+                    res.end(data);
+                });
+            }
+        };
+    }
+}
+
+module.exports = FeDevServer;
